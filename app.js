220cf282e272fca8221a4caee1b47eb8d371cf72
Pushkar Sabe <pushkarsabe@gmail.com>
2025-09-22 20:42:13 +0530

    db chnages

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const sequelize = require('./util/database');
-
+                          
 const app = express();
 app.use(cors());
 
